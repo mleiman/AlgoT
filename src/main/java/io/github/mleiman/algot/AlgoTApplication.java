29979accf0093fec00d73e0aaa2681887d86abf3
mleiman <marcin.leiman@gmail.com>
2026-02-26 17:10:07 +0000

    project init

---
+++ src/main/java/io/github/mleiman/algot/AlgoTApplication.java
@@ -0,0 +1,13 @@
+package io.github.mleiman.algot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoTApplication.class, args);
+    }
+
+}
